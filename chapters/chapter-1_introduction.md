080e72772a02f16285b336dcb57e5ca477b281c3
jyoti singh <jyotisinghcse2015@gmail.com>
2026-07-30 19:05:57 +0530

    react notes

---
+++ chapters/chapter-1_introduction.md
@@ -0,0 +1,15 @@
+## 1. React kya hai?
+- React ek JavaScript Library hai jo User Interface (UI) banane ke liye use hoti hai.
+- Update only what changed.
+
+## Library
+- Library sirf ek specific problem solve karti hai.
+
+**Example:**
+UI banana
+
+## Framework
+- Framework poori application ka structure decide karta hai.
+
+**Example:**
+Angular
---
+++ js/chapters.js
@@ -0,0 +1,129 @@
+const chapters = [
+    {
+        id: 1,
+        title: "Introduction to React",
+        file: "chapter-1_introduction.md"
+    },
+    {
+        id: 2,
+        title: "Data Types",
+        file: "chapter-2_data-type.md"
+    },
+    {
+        id: 3,
+        title: "Template Literals",
+        file: "chapter-3_template-literals.md"
+    },
+    {
+        id: 4,
+        title: "Decision Making",
+        file: "chapter-4_decision-making.md"
+    },
+    {
+        id: 5,
+        title: "Loop",
+        file: "chapter-5_loop.md"
+    },
+    {
+        id: 6,
+        title: "Functions",
+        file: "chapter-6_functions.md"
+    },
+    {
+        id: 7,
+        title: "Arrays",
+        file: "chapter-7_arrays.md"
+    },
+    {
+        id: 8,
+        title: "Objects & Destructuring",
+        file: "chapter-8_objects.md"
+    },
+    {
+        id: 9,
+        title: "ES6 Features (Spread, Rest, Optional Chaining, Nullish Coalescing)",
+        file: "chapter-9_ES6-features.md"
+    },
+    {
+        id: 10,
+        title: "Modules (import / export)",
+        file: "chapter-10_modules.md"
+    },
+    {
+        id: 11,
+        title: "Promises",
+        file: "chapter-11_promises.md"
+    },
+    {
+        id: 12,
+        title: "Async / Await",
+        file: "chapter-12_async-await.md"
+    },
+    {
+        id: 13,
+        title: "Fetch API",
+        file: "chapter-13_fetch-api.md"
+    },
+    {
+        id: 14,
+        title: "Pratice Task",
+        file: "chapter-14_task.md"
+    },
+    {
+        id: 15,
+        title: "Algorithm",
+        file: "chapter-15_algorithm.md"
+    },
+    {
+        id: 16,
+        title: "Logic Building Level1",
+        file: "chapter-16_logic-building-level1.md"
+    },
+    {
+        id: 17,
+        title: "Logic Building Level2",
+        file: "chapter-17_logic-building-level2.md"
+    },
+    {
+        id: 18,
+        title: "Logic Building Level3",
+        file: "chapter-18_logic-building-level3.md"
+    },
+    {
+        id: 19,
+        title: "Logic Building Level4",
+        file: "chapter-19_logic-building-level4.md"
+    },
+    {
+        id: 20,
+        title: "Logic Building Level5",
+        file: "chapter-20_logic-building-level5.md"
+    },
+    {
+        id: 21,
+        title: "Logic Building Level6",
+        file: "chapter-21_logic-building-level6.md"
+    },
+    {
+        id: 22,
+        title: "Logic Building Level7",
+        file: "chapter-22_logic-building-level7.md"
+    },
+    {
+        id: 23,
+        title: "Logic Building Level8",
+        file: "chapter-23_logic-building-level8.md"
+    },
+    {
+        id: 24,
+        title: "Logic Building Level9",
+        file: "chapter-24_logic-building-level9.md"
+    },
+    {
+        id: 25,
+        title: "Logic Building Level10",
+        file: "chapter-25_logic-building-level10.md"
+    }
+
+    
+];
